162436a643aeae59fe2acdfbef1e86fb7a8dcf0f
priyasri01 <Priyasri.april14@gmail.com>
2022-12-21 22:23:22 -0500

    I added my dog class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi guys");
+
+        System.out.println("Hello Everyone");
+        Dog xx = new Dog();
+        xx.breed= "Husky";
+        xx.color="Black & White";
     }
 }
