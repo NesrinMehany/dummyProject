527686200410b3fcecb6426500da8053ece5940c
aliilhann <alirizailhan89@gmail.com>
2022-12-21 21:24:01 -0600

    rabbit class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hi guys");
+        Rabbit rabbit=new Rabbit();
+        rabbit.name="bunny";
+        rabbit.height=1.8;
+        rabbit.weight=4;
+        rabbit.pretty= true;
     }
 }
