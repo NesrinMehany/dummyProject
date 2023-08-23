6a9729d58453ceb1acdce59c042725f85d81d428
NesrinMehany <nesreen.safwat87@gmail.com>
2023-08-23 15:21:41 -0400

    nesreen's trial

--- src/CatClass.java
+++ src/CatClass.java
@@ -2,4 +2,9 @@ public class CatClass {
     String name;
     String colour;
     int age;
+
+    public static void main(String[] args) {
+        System.out.println("Nesreen hates cats");
+    }
+
 }
